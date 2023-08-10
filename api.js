ce15beeccc213014d08a307c4064f5e79cb042d4
Jbsmall17 <alaomiyaki17@gmail.com>
2023-08-09 23:32:24 -0700

    changes a few lines of code in the block code of the if satement with conditions:req.url.startsWith('/v1/students/') && req.method === 'PATCH', in the handleRequest function

--- api.js
+++ api.js
@@ -69,9 +69,10 @@ const handleRequest = (req, res) => { // handles all requests, routing and metho
         }
 
 
-        const student = { ...students[studentIndex], ...req.body } // updates the student information
-
-        return response({ data: student, code: 200 }) // return response
+        const student = { ...students[studentIndex],id: Math.floor(Math.random() * 10).toString()} //, updates the student information
+        students.splice(studentIndex,1,student) //removes the oldstudent info and replaced it with updated info
+        
+        return response({ data: students, code: 200 }) // return response
 
     }
 
